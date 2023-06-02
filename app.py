ada6ae09f91622dd1518e2e5c01e963f719a373d
nakarin <nakarinl@ais.co.th>
2023-06-02 16:19:12 +0700

    Add Files

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def cal_range():
     begin_latlng = (float(begin_lat),float(begin_lng))
     end_latlng = (float(end_lat),float(end_lng))
 
-    G = ox.graph_from_point(start_latlng, dist=2000, network_type='drive', simplify=False) 
-    orig_node = ox.nearest_nodes(G, start_latlng[1], start_latlng[0])
+    G = ox.graph_from_point(begin_latlng, dist=2000, network_type='drive', simplify=False) 
+    orig_node = ox.nearest_nodes(G, begin_latlng[1], begin_latlng[0])
     dest_node = ox.nearest_nodes(G, end_latlng[1], end_latlng[0])
     shortest_route = nx.shortest_path(G, orig_node, dest_node, method='bellman-ford',weight='length')
     lenght = nx.shortest_path_length(G, orig_node, dest_node, weight='length')
